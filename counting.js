5c40fa362571a03ee4ce11b5dac07f833f8aba69
cade.gardner <d00299297@dmail.dixie.edu>
2020-05-05 10:45:00 -0600

    stuff

--- counting.js
+++ counting.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 console.log('connected');
 
 var app = new Vue({
@@ -5,15 +7,55 @@ var app = new Vue({
     vuetify: new Vuetify(),
     data: {
         page: 'login', // login, loggedin
-        parks: ['Acadia', 'America Samoa', 'Arches', 'Badlands', 'Big Bend', 'Biscayne', 'Black Canyon of the Gunnison', 'Bryce Canyon', 'Canyon Lands' ],
+        parks: ['Acadia', 'America Samoa', 'Arches', 'Badlands', 'Big Bend', 'Biscayne', 'Black Canyon of the Gunnison', 'Bryce Canyon', 'Canyon Lands', 'Zion National Park' ],
+        parkEmpty: false,
+        parkField: '',
         passEmpty: false,
-        passField: ''
+        passField: '',
+
+        todaysDate: "",
+        currentPark: "",
+    },
+    created: function(){
+        this.getTodaysDate();
     },
     methods: {
+        getTodaysDate: function () {
+            var date = new Date();
+            var days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+            var months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
+            var fulldate = days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDate() + " " + date.getFullYear();
+            this.todaysDate = fulldate;
+            console.log(this.todaysDate);
+        },
+        validateRequestData: function(){
+            if (this.parkField == ""){
+                this.parkEmpty = true;
+            }else{
+                this.parkEmpty = false;
+            }
+            if (this.passField == ""){
+                this.passEmpty = true;
+            }else{
+                this.passEmpty = false;
+            }
+            if (this.parkEmpty == true || this.passEmpty == true){
+                return false;
+            }else{
+                return true;
+            }
+        },
         loginClicked: function(){
-            console.log('clicked');
-        }
+            if(this.validateRequestData()){
+                this.currentPark = this.parkField;
+                this.page = "loggedin";
+                this.parkField = "";
+                this.passField = "";
+            }
+        },
     }
+
 
 
 
